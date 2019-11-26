c18ffe74cdbd2d2f45b2c0b1f216270b962f7327
Gohar Dovlatyan <38962458+dovlatyangohar@users.noreply.github.com>
2019-11-26 23:14:38 +0400

    Update Main.kt

--- src/Main.kt
+++ src/Main.kt
@@ -20,3 +20,30 @@ fun main() {
     println(age)
     println(name)
 }
+
+//right version
+//import java.lang.ref.WeakReference
+//import kotlin.properties.ReadWriteProperty
+//import kotlin.reflect.KProperty
+//
+//class WeakRefExample<R, T>(private var value: WeakReference<T?>) : ReadWriteProperty<R, T?> {
+//
+//    override fun getValue(thisRef: R, property: KProperty<*>): T? {
+//        return value.get()
+//    }
+//
+//    override fun setValue(thisRef: R, property: KProperty<*>, value: T?) {
+//        println("$value has been assigned to '${property.name}' in $thisRef.")
+//    }
+//
+//
+//}
+//
+//fun <R, T> weak(value: T) = WeakRefExample<R, T>(WeakReference(value))
+//fun main() {
+//
+//    val name by weak("Jack")
+//    val age by weak(45)
+//    println(age)
+//    println(name)
+//}
